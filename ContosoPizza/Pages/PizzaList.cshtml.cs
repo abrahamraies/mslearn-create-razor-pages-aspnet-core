4d5cd7eb9c3d4f173fa5b331ededdccda1b29163
abrahamraies <abrahamraies@gmail.com>
2024-10-01 19:33:24 +1000

    myFirstAppWithRazor

---
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+
+namespace ContosoPizza.Pages
+{
+    public class PizzaListModel(PizzaService service) : PageModel
+    {
+        private readonly PizzaService _service = service;
+        public IList<Pizza> PizzaList { get; set; } = default!;
+
+        public void OnGet()
+        {
+            PizzaList = _service.GetPizzas();
+        }
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
+        }
+    }
+}
